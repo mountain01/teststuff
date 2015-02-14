e4ffbcc0dbd43791a68bde8a7aa9acc4552d325a
Matt Edwards <edwardsjm01@gmail.com>
2015-02-14 13:57:08 -0700

    first test

--- src/prefix.js
+++ src/prefix.js
@@ -2,4 +2,29 @@ var app = angular.module('myApp',[]);
 
 app.controller('myController',['$scope',function($scope){
 	$scope.mystuff = "hello there";
+
+	$scope.infixToPrefix = function(inFix){
+		var myStack = [];
+		var input = inFix.split(" ");
+		var operators = ['+','-','/','%','*'];
+		for(var i = 0; i < input.length;i++){
+			if(operators.indexOf(input[i]) == -1){
+				myStack.push(input[i]);
+			}
+			else {
+				myStack.push(input[i+1]);
+				myStack.push(input[i]);
+				i+=2;
+			}
+		}
+		var out ="";
+		myStack.forEach(function(item,index){
+			out+=item;
+			if(index != myStack.length-1){
+				out+=" ";
+			}
+		});
+		$scope.answer = out;
+
+	}
 }]);
